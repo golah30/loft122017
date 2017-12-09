ddde0db04477a221da5ba0fbd7a72c3be32585e7
Dima Kurilenko <golah30@gmail.com>
2017-12-09 19:11:50 +0200

    init repo and workflow

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+const gulp = require('gulp');
+
+const pug = require('gulp-pug');
+
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+const del = require('del');
+
+const browserSync = require('browser-sync').create();
+
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+
+const babel = require('gulp-babel');
+
+const paths = {
+  dest: './build',
+  pug: {
+    pages: './src/pug/pages/*.pug',
+    src: './src/pug/**/*.pug'
+  },
+  scss: {
+    src: './src/scss/main.scss',
+    watch: './src/scss/**/*.scss',
+    dest: './build/styles/'
+  },
+  img: {
+    src: './src/img/**/*.*',
+    dest: './build/img/'
+  },
+  scripts: {
+    src: './src/js/**/*.js',
+    dest: './build/js/'
+  }
+};
+
+//html
+function pages() {
+  return gulp
+    .src(paths.pug.pages)
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(paths.dest));
+}
+
+//styles ++ postcss ( autoprefixer )
+function styles() {
+  return gulp
+    .src(paths.scss.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scss.dest));
+}
+
+// clean dest dir
+function clean() {
+  return del(paths.dest);
+}
+
+// replace img
+function img() {
+  return gulp.src(paths.img.src).pipe(gulp.dest(paths.img.dest));
+}
+
+// webpack + gulp-babel
+function scripts() {
+  return gulp
+    .src('src/js/main.js')
+    .pipe(gulpWebpack(webpackConfig, webpack))
+    .pipe(babel())
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// галповский вотчер
+function watch() {
+  gulp.watch(paths.scss.watch, styles);
+  gulp.watch(paths.pug.src, pages);
+  gulp.watch(paths.img.src, img);
+  gulp.watch(paths.scripts.src, scripts);
+}
+
+// локальный сервер + livereload (встроенный)
+function server() {
+  browserSync.init({
+    server: paths.dest
+  });
+  browserSync.watch(paths.dest + '/**/*.*', browserSync.reload);
+}
+
+gulp.task(
+  'default',
+  gulp.series(
+    clean,
+    gulp.parallel(styles, pages, img, scripts),
+    gulp.parallel(watch, server)
+  )
+);
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+const mod = require('./modules/console.js');
+
+console.log(1);
+mod(2);
