bd093d005a856f7f215648197a409e4434be1d77
Dima Kurilenko <golah30@gmail.com>
2017-12-29 16:10:03 +0200

    added adapt scss to about page, footer, header

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 const mod = require('./modules/console.js');
 const slider = require('./modules/slider.js');
-
-slider();
+if (document.querySelector('.slider')) slider();
 
 var map = new google.maps.Map(document.querySelector('.about-contacts__map'), {
   center: { lat: 48.575, lng: 35.09 },
