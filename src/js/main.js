198aa5db3329eab2aeee15a1a0544c59bb5354d1
Dima Kurilenko <golah30@gmail.com>
2018-01-11 19:50:25 +0200

    modified hamburger menu animation

--- src/js/main.js
+++ src/js/main.js
@@ -14,50 +14,50 @@ window.onload = function() {
   if (document.querySelector('.container-blog')) blog();
   if (document.querySelector('.slider')) slider();
   if (document.querySelector('.flip')) flip();
+  if (document.querySelector('.about-contacts__map'))
+    var map = new google.maps.Map(
+      document.querySelector('.about-contacts__map'),
+      {
+        center: { lat: 48.575, lng: 35.09 },
+        zoom: 17,
+        scrollwheel: false,
+        styles: [
+          { elementType: 'geometry', stylers: [{ color: '#ffffff' }] },
+          {
+            featureType: 'landscape',
+            elementType: 'labels',
+            stylers: [{ visibility: 'off' }]
+          },
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#111111' }]
+          },
+          { elementType: 'labels.text.fill', stylers: [{ color: '#a6a6a6' }] },
+
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{ color: '#eaeaea' }]
+          },
+
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#c2c2c2' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{ color: '#61dac9' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#c2c2c2' }]
+          }
+        ],
+        disableDefaultUI: true
+      }
+    );
 };
-if (document.querySelector('.about-contacts__map'))
-  var map = new google.maps.Map(
-    document.querySelector('.about-contacts__map'),
-    {
-      center: { lat: 48.575, lng: 35.09 },
-      zoom: 17,
-      scrollwheel: false,
-      styles: [
-        { elementType: 'geometry', stylers: [{ color: '#ffffff' }] },
-        {
-          featureType: 'landscape',
-          elementType: 'labels',
-          stylers: [{ visibility: 'off' }]
-        },
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#111111' }]
-        },
-        { elementType: 'labels.text.fill', stylers: [{ color: '#a6a6a6' }] },
-
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{ color: '#eaeaea' }]
-        },
-
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#c2c2c2' }]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{ color: '#61dac9' }]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{ color: '#c2c2c2' }]
-        }
-      ],
-      disableDefaultUI: true
-    }
-  );
--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -2,7 +2,8 @@ function activate(e) {
   const container = document.querySelector('.hamburger-menu'),
     bgl = container.querySelector('.hamburger__bg-left'),
     bgr = container.querySelector('.hamburger__bg-right'),
-    list = container.querySelector('.hamburger-menu__list');
+    list = container.querySelector('.hamburger-menu__list'),
+    items = container.querySelectorAll('.hamburger-menu__item');
 
   container.classList.add('hamburger-menu--active');
 
@@ -10,6 +11,13 @@ function activate(e) {
     bgl.classList.add('hamburger__bg-left--active');
     bgr.classList.add('hamburger__bg-right--active');
     list.classList.add('hamburger-menu__list--active');
+
+    let delay = 1000 / items.length;
+    for (let i = 0; i < items.length; ++i) {
+      setTimeout(function() {
+        items[i].classList.add('hamburger-menu__item--active');
+      }, delay * i + 2);
+    }
   }, 100);
 }
 
@@ -17,14 +25,25 @@ function close(e) {
   const container = document.querySelector('.hamburger-menu'),
     bgl = container.querySelector('.hamburger__bg-left'),
     bgr = container.querySelector('.hamburger__bg-right'),
-    list = container.querySelector('.hamburger-menu__list');
+    list = container.querySelector('.hamburger-menu__list'),
+    items = container.querySelectorAll('.hamburger-menu__item');
 
-  bgl.classList.remove('hamburger__bg-left--active');
-  bgr.classList.remove('hamburger__bg-right--active');
-  list.classList.remove('hamburger-menu__list--active');
-  setTimeout(function() {
-    container.classList.remove('hamburger-menu--active');
-  }, 300);
+  let delay = 500 / items.length;
+  for (let i = 0; i < items.length; ++i) {
+    setTimeout(function() {
+      let index = items.length - i - 1;
+      items[index].classList.remove('hamburger-menu__item--active');
+
+      if (i == items.length - 1) {
+        bgl.classList.remove('hamburger__bg-left--active');
+        bgr.classList.remove('hamburger__bg-right--active');
+        list.classList.remove('hamburger-menu__list--active');
+        setTimeout(function() {
+          container.classList.remove('hamburger-menu--active');
+        }, 300);
+      }
+    }, delay * i + 2);
+  }
 }
 
 module.exports = function() {
